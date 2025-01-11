aa740b9f12d9009c5c718fffa23eff8719b4bb36
dare-devil-ex <kmsedition@gmail.com>
2025-01-11 17:41:10 +0530

    modified:   wkaie.py

--- wkaie.py
+++ wkaie.py
@@ -1,3 +1,16 @@
-
+import requests as r
 
 url = "https://random-hadith-generator.vercel.app/bukhari/"
+load = r.get(url).json()
+
+book = str(load['data']["book"])
+bookName = str(load['data']["bookName"]).replace("\t", "").replace("\n", "")
+chapter = str(load['data']["chapterName"]).replace("Chapter:", "").replace("\n", "")
+ref = str(load['data']["refno"])
+hadith = str(load['data']["hadith_english"])
+
+print("Book:", book)
+print("Book name:", bookName)
+print("Chapter name:", chapter)
+print("Reference no:", ref)
+print("Hadith:", hadith)
