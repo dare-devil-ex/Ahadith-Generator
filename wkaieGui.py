03a4099af40fb7f946583e7a0dd95a88eca2077a
dare-devil-ex <kmsedition@gmail.com>
2025-01-12 13:26:09 +0530

    modified:   wkaieGui.py

--- wkaieGui.py
+++ wkaieGui.py
@@ -13,19 +13,11 @@ class Wkaie:
         self.generator()
 
     def generator(self):
-        self.url = "https://random-hadith-generator.vercel.app/bukhari/"
-        self.load = r.get(self.url).json()
-        self.book = str(self.load['data']["book"])
-        self.bookName = str(self.load['data']["bookName"]).replace("\t", "").replace("\n", "")
-        self.chapter = str(self.load['data']["chapterName"]).replace("Chapter:", "").replace("\n", "")
-        self.ref = str(self.load['data']["refno"])
-        self.hadith = str(self.load['data']["hadith_english"])
         self.Lbook = Label(self.root, text="Book:", font=("Helvetica", 14))
         self.LbookName = Label(self.root, text="Book name:", font=("Helvetica", 14))
         self.LchapterName = Label(self.root, text="Chapter name:", font=("Helvetica", 14))
         self.LrefNo = Label(self.root, text="Reference no:", font=("Helvetica", 14))
         self.Lhadith = Label(self.root, text="Hadith:", wraplength=1050, font=("Helvetica", 14))
-        
         self.LbookName.pack(pady=6)
         self.Lbook.pack(pady=5)
         self.LchapterName.pack(pady=7)
@@ -36,6 +28,15 @@ class Wkaie:
         button.pack(pady=1)
         
     def wkaie(self):
+        self.url = "https://random-hadith-generator.vercel.app/bukhari/"
+        self.load = r.get(self.url).json()
+        
+        self.book = str(self.load['data']["book"])
+        self.bookName = str(self.load['data']["bookName"]).replace("\t", "").replace("\n", "")
+        self.chapter = str(self.load['data']["chapterName"]).replace("Chapter:", "").replace("\n", "")
+        self.ref = str(self.load['data']["refno"])
+        self.hadith = str(self.load['data']["hadith_english"])
+        
         self.LbookName.config(text=f"Book: {self.book}")
         self.Lbook.config(text=f"Book name: {self.bookName}")
         self.LchapterName.config(text=f"Chapter name: {self.chapter}")
